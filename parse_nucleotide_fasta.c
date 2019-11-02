03acd1d2c44c25ce5291b974d0fe29343ce9ddc7
Thomas Bujaki <ofbujak@gmail.com>
2019-11-01 20:57:18 -0400

    made flag calls more clear

--- parse_nucleotide_fasta.c
+++ parse_nucleotide_fasta.c
@@ -1,12 +1,12 @@
+#include <getopt.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <getopt.h>
 #include <string.h>
-#include <stdbool.h>
 
-typedef struct Flags {  // flag 0: file, flag 1: gc, flag 2: out file, flag 3: match, flag 4: merge, flag 5: seq.
-    bool flag_raised;
-    char flag_value[100];
+typedef struct Flags {
+    bool raised;
+    char value[100];
 } Flags;
 
 typedef struct All_flags {
@@ -35,10 +35,9 @@ typedef struct Files {
     FILE* out_file;
 } Files;
 
-
 /******************************
-*   Changes input into uppercase. 
-*******************************/
+ *   Changes input into uppercase.
+ *******************************/
 void change_lowercase(char* text) {
     int i;
     for (i = 0; i < strlen(text); i++) {
@@ -49,9 +48,9 @@ void change_lowercase(char* text) {
 }
 
 /******************************
-*   Parses the input of the -seq option and splits the input by ,. 
-*       If the sequence text passed in contains the same 
-*******************************/
+ *   Parses the input of the -seq option and splits the input by ,.
+ *       If the sequence text passed in contains the same
+ *******************************/
 bool does_header_match(char* seq_search_string, char* curr_read, bool* prev_header) {
     if (curr_read[0] != '>') {
         return false;
@@ -70,24 +69,15 @@ bool does_header_match(char* seq_search_string, char* curr_read, bool* prev_head
     }
 }
 /******************************
-*   Ensures input is workable and parses the input for the settings the program should use.
-*******************************/
+ *   Ensures input is workable and parses the input for the settings the program should use.
+ *******************************/
 int ensure_legal_arguments(int argcount, char** argvalues, struct All_flags* all_flags) {
     if (argcount == 1) {
         printf("No arguments entered, please see maunal or enter ./parse_nucleotide_fasta.out -help\n");
         return -1;
     }
 
-    static struct option long_options[] = {
-        {"file", required_argument, NULL, 'f'},
-        {"gc", optional_argument, NULL, 'c'},
-        {"match", required_argument, NULL, 'm'},
-        {"merge", required_argument, NULL, 'g'},
-        {"seq", required_argument, NULL, 's'},
-        {"out", required_argument, NULL, 'o'},
-        {"help", no_argument, NULL, 'h'},
-        {NULL, 0, NULL, 0}
-    };
+    static struct option long_options[] = {{"file", required_argument, NULL, 'f'}, {"gc", optional_argument, NULL, 'c'}, {"match", required_argument, NULL, 'm'}, {"merge", required_argument, NULL, 'g'}, {"seq", required_argument, NULL, 's'}, {"out", required_argument, NULL, 'o'}, {"help", no_argument, NULL, 'h'}, {NULL, 0, NULL, 0}};
 
     int opts;
     int argument_legality = -1;
@@ -95,32 +85,32 @@ int ensure_legal_arguments(int argcount, char** argvalues, struct All_flags* all
         switch (opts) {
             case 'f':
                 argument_legality = 0;
-                all_flags->file_in.flag_raised = true;
-                strcpy(all_flags->file_in.flag_value, optarg);
+                all_flags->file_in.raised = true;
+                strcpy(all_flags->file_in.value, optarg);
                 printf("File %s passed in.\n", optarg);
                 break;
             case 'c':
                 argument_legality = 0;
-                all_flags->gc.flag_raised = true;
+                all_flags->gc.raised = true;
                 if (optarg == NULL) {
                     printf("Calculating global GC content.\n");
                 } else {
-                    strcpy(all_flags->gc.flag_value, optarg);
+                    strcpy(all_flags->gc.value, optarg);
                     printf("Calculating GC content per %s nucleotides\n", optarg);
                 }
                 break;
             case 'o':
                 argument_legality = 0;
-                all_flags->file_out.flag_raised = true;
-                strcpy(all_flags->file_out.flag_value, optarg);
-                printf("Printing output into file %s.\n", all_flags->file_out.flag_value);
+                all_flags->file_out.raised = true;
+                strcpy(all_flags->file_out.value, optarg);
+                printf("Printing output into file %s.\n", all_flags->file_out.value);
                 break;
             case 'm':
                 argument_legality = 0;
-                all_flags->match.flag_raised = true;
-                strcpy(all_flags->match.flag_value, optarg);
-                change_lowercase(all_flags->match.flag_value);
-                printf("Searching for all instances of %s in file.\n", all_flags->match.flag_value);
+                all_flags->match.raised = true;
+                strcpy(all_flags->match.value, optarg);
+                change_lowercase(all_flags->match.value);
+                printf("Searching for all instances of %s in file.\n", all_flags->match.value);
                 break;
             case 'h':
                 printf("Parse Nucleotide Fasta Options: \n");
@@ -138,15 +128,15 @@ int ensure_legal_arguments(int argcount, char** argvalues, struct All_flags* all
                 break;
             case 'g':
                 argument_legality = 0;
-                all_flags->merge.flag_raised = true;
-                strcpy(all_flags->merge.flag_value, optarg);
-                printf("Merging sequences into file: %s.\n", all_flags->merge.flag_value);
+                all_flags->merge.raised = true;
+                strcpy(all_flags->merge.value, optarg);
+                printf("Merging sequences into file: %s.\n", all_flags->merge.value);
                 break;
             case 's':
                 argument_legality = 0;
-                all_flags->seq.flag_raised = true;
-                strcpy(all_flags->seq.flag_value, optarg);
-                printf("Carrying out operations only on selected sequences using sequence identifier string: %s.\n", all_flags->seq.flag_value);
+                all_flags->seq.raised = true;
+                strcpy(all_flags->seq.value, optarg);
+                printf("Carrying out operations only on selected sequences using sequence identifier string: %s.\n", all_flags->seq.value);
                 break;
             case '?':
                 printf("Unknown or incorrect argument entered, exiting program. Please see maunal or enter ./parse_nucleotide_fasta.out -help\n");
@@ -158,14 +148,10 @@ int ensure_legal_arguments(int argcount, char** argvalues, struct All_flags* all
                 break;
         }
     }
-    if (all_flags->file_in.flag_raised == false) {
+    if (all_flags->file_in.raised == false) {
         printf("A fasta file must be passed in. Please see maunal or enter ./parse_nucleotide_fasta.out -help\n");
         argument_legality = -1;
-    } else if (all_flags->gc.flag_raised == false &&
-               all_flags->file_out.flag_raised == false &&
-               all_flags->match.flag_raised == false &&
-               all_flags->merge.flag_raised == false &&
-               all_flags->seq.flag_raised == false) {
+    } else if (all_flags->gc.raised == false && all_flags->file_out.raised == false && all_flags->match.raised == false && all_flags->merge.raised == false && all_flags->seq.raised == false) {
         printf("No process arguments entered, select a process for the program carry out. Please see maunal or enter ./parse_nucleotide_fasta.out -help\n");
         argument_legality = -1;
     }
@@ -173,30 +159,30 @@ int ensure_legal_arguments(int argcount, char** argvalues, struct All_flags* all
 }
 
 /******************************
-*   Prints output to either command line or to the file passed in from -out <file>.
-*******************************/
-void print_output(struct All_flags* all_flags, struct GC_data* gc_data, int occurence_count, struct Files* files, bool called_from_gc_func) {
+ *   Prints output to either command line or to the file passed in from -out <file>.
+ *******************************/
+void print_output(All_flags* all_flags, GC_data* gc_data, int occurence_count, Files* files, bool called_from_gc_func) {
     if (called_from_gc_func != false) {
         fprintf(files->out_file, "Chunk number: %d A: %d T: %d C: %d G:%d\n", gc_data->chunk_num, gc_data->site_values[0], gc_data->site_values[1], gc_data->site_values[2], gc_data->site_values[3]);
         fprintf(files->out_file, "Chunk: %d GC content: %f\n\n", gc_data->chunk_num, (((float)gc_data->site_values[2] + (float)gc_data->site_values[3]) / (float)gc_data->site_values[4]) * 100);
-        } else {
-            if (all_flags->gc.flag_raised != false) {
-                fprintf(files->out_file, "Total size of final chunk: %d. Chunk num: %d A: %d T: %d C: %d G:%d\n", gc_data->site_values[4], gc_data->chunk_num, gc_data->site_values[0], gc_data->site_values[1], gc_data->site_values[2], gc_data->site_values[3]);
-                fprintf(files->out_file, "Chunk: %d GC content: %f\n\n", gc_data->chunk_num, (((float)gc_data->site_values[2] + (float)gc_data->site_values[3]) / (float)gc_data->site_values[4]) * 100);
-            }
-            if (all_flags->match.flag_raised != false) { // need different file output here
-                fprintf(files->out_file, "Total number of occurences of %s in the file was %d\n\n", all_flags->match.flag_value, occurence_count);
-            }
+    } else {
+        if (all_flags->gc.raised != false) {
+            fprintf(files->out_file, "Total size of final chunk: %d. Chunk num: %d A: %d T: %d C: %d G:%d\n", gc_data->site_values[4], gc_data->chunk_num, gc_data->site_values[0], gc_data->site_values[1], gc_data->site_values[2], gc_data->site_values[3]);
+            fprintf(files->out_file, "Chunk: %d GC content: %f\n\n", gc_data->chunk_num, (((float)gc_data->site_values[2] + (float)gc_data->site_values[3]) / (float)gc_data->site_values[4]) * 100);
         }
+        if (all_flags->match.raised != false) {  // need different file output here
+            fprintf(files->out_file, "Total number of occurences of %s in the file was %d\n\n", all_flags->match.value, occurence_count);
+        }
+    }
 }
 
 /******************************
-*   Counts the GC content in the read passed into the function. 
-*******************************/
-void GC_count(struct All_flags* all_flags, char* read, struct GC_data* gc_data, struct Files* files) {
+ *   Counts the GC content in the read passed into the function.
+ *******************************/
+void GC_count(All_flags* all_flags, char* read, GC_data* gc_data, Files* files) {
     int site_index = 0;
     while (read[site_index] != '\0') {
-        if(gc_data->site_values[4] == gc_data->chunk_size && gc_data->chunk_size != 0) {
+        if (gc_data->site_values[4] == gc_data->chunk_size && gc_data->chunk_size != 0) {
             print_output(all_flags, gc_data, 0, files, true);
             int i;
             for (i = 0; i < 5; i++) {
@@ -218,30 +204,30 @@ void GC_count(struct All_flags* all_flags, char* read, struct GC_data* gc_data, 
             gc_data->site_values[3]++;
             gc_data->site_values[4]++;
         } else {
-          // Do Nothing
+            // Do Nothing
         }
         site_index++;
     }
 }
 
 /******************************
-*   Searches recursivly the bases in two reads (the current read that the program is on and the prevous read).
-*   It will output 1 if the search gets to the first index in the word without matching errors (meaning the
-*       read and the word match) and 0 otherwise.
-*******************************/
-int recursive_char_search(struct All_flags* all_flags, int word_index, char* curr_read, char* prev_read, int index) {
+ *   Searches recursivly the bases in two reads (the current read that the program is on and the prevous read).
+ *   It will output 1 if the search gets to the first index in the word without matching errors (meaning the
+ *       read and the word match) and 0 otherwise.
+ *******************************/
+int recursive_char_search(All_flags* all_flags, int word_index, char* curr_read, char* prev_read, int index) {
     if (word_index < 0) {
         return 1;
     }
     if (index < 0) {
         int prev_read_size = strlen(prev_read);
-        if (prev_read[prev_read_size + index] == all_flags->match.flag_value[word_index]) {
+        if (prev_read[prev_read_size + index] == all_flags->match.value[word_index]) {
             return recursive_char_search(all_flags, word_index - 1, curr_read, prev_read, index - 1);
         } else {
             return 0;
         }
     } else {
-        if (curr_read[index] == all_flags->match.flag_value[word_index]) {
+        if (curr_read[index] == all_flags->match.value[word_index]) {
             return recursive_char_search(all_flags, word_index - 1, curr_read, prev_read, index - 1);
         } else {
             return 0;
@@ -250,11 +236,11 @@ int recursive_char_search(struct All_flags* all_flags, int word_index, char* cur
 }
 
 /******************************
-*   Counts the number of times a word is found in the fasta file
-*******************************/
-void matching_occurences_count(struct All_flags* all_flags, char* curr_read, char* prev_read, int* occurence_count) {
+ *   Counts the number of times a word is found in the fasta file
+ *******************************/
+void matching_occurences_count(All_flags* all_flags, char* curr_read, char* prev_read, int* occurence_count) {
     int curr_read_size = strlen(curr_read);
-    int word_size = strlen(all_flags->match.flag_value);
+    int word_size = strlen(all_flags->match.value);
     int i;
     if (strcmp(prev_read, "")) {
         for (i = 0; i < curr_read_size; i++) {
@@ -268,15 +254,13 @@ void matching_occurences_count(struct All_flags* all_flags, char* curr_read, cha
 }
 
 /******************************
-*   Iterates over the lines of the opened fasta file.
-*******************************/
-void iterate_over_lines(struct Files* files, struct All_flags* all_flags, struct Reads* reads, struct GC_data* gc_data, int* occurence_count) {
+ *   Iterates over the lines of the opened fasta file.
+ *******************************/
+void iterate_over_lines(Files* files, All_flags* all_flags, Reads* reads, GC_data* gc_data, int* occurence_count) {
     while (!feof(files->fasta_file)) {
         fscanf(files->fasta_file, "%s\n", reads->curr_read);
         bool prev_header = false;
-        if (all_flags->seq.flag_raised == false ||
-            does_header_match(all_flags->seq.flag_value, reads->curr_read, &prev_header) != false ||
-            prev_header != false) {
+        if (all_flags->seq.raised == false || does_header_match(all_flags->seq.value, reads->curr_read, &prev_header) != false || prev_header != false) {
             if (reads->curr_read[0] == '>') {
                 strcpy(reads->prev_read, "");
                 fscanf(files->fasta_file, "%s\n", reads->curr_read);
@@ -285,10 +269,10 @@ void iterate_over_lines(struct Files* files, struct All_flags* all_flags, struct
                 fprintf(files->merge_file, "%s\n", reads->curr_read);
             }
             change_lowercase(reads->curr_read);
-            if (all_flags->gc.flag_raised != false) {  // gc
+            if (all_flags->gc.raised != false) {  // gc
                 GC_count(all_flags, reads->curr_read, gc_data, files);
             }
-            if (all_flags->match.flag_raised != false) {  // match
+            if (all_flags->match.raised != false) {  // match
                 matching_occurences_count(all_flags, reads->curr_read, reads->prev_read, occurence_count);
                 strcpy(reads->prev_read, reads->curr_read);
             }
@@ -297,37 +281,37 @@ void iterate_over_lines(struct Files* files, struct All_flags* all_flags, struct
 }
 
 /******************************
-*   Reads through fasta file and runs other functions depending on the input flags passed in.
-*       This function opens the file and reads through each line, It will pass the lines to other functions which do stuff
-*******************************/
-void parse_fasta(struct All_flags* all_flags) {
-    struct Reads reads;
+ *   Reads through fasta file and runs other functions depending on the input flags passed in.
+ *       This function opens the file and reads through each line, It will pass the lines to other functions which do stuff
+ *******************************/
+void parse_fasta(All_flags* all_flags) {
+    Reads reads;
     strcpy(reads.curr_read, "");
     strcpy(reads.prev_read, "");
 
-    struct GC_data gc_data;
+    GC_data gc_data;
     gc_data.chunk_num = 1;
-    gc_data.chunk_size = strtol(all_flags->gc.flag_value, NULL, 10);
+    gc_data.chunk_size = strtol(all_flags->gc.value, NULL, 10);
     int i;
     for (i = 0; i < 5; i++) {
         gc_data.site_values[i] = 0;
     }
 
     int occurence_count = 0;
-    struct Files files;
-    files.fasta_file = fopen(all_flags->file_in.flag_value, "r");
+    Files files;
+    files.fasta_file = fopen(all_flags->file_in.value, "r");
     if (files.fasta_file == NULL) {
-        printf("The fasta file %s does not appear to exist. Exiting program.\n", all_flags->file_in.flag_value);
+        printf("The fasta file %s does not appear to exist. Exiting program.\n", all_flags->file_in.value);
         return;
     }
 
-    if (all_flags->file_out.flag_raised != false) {
-        files.out_file = fopen(all_flags->file_out.flag_value, "w");
-    }else { 
+    if (all_flags->file_out.raised != false) {
+        files.out_file = fopen(all_flags->file_out.value, "w");
+    } else {
         files.out_file = stdout;
     }
-    if (all_flags->merge.flag_raised != false) {
-        files.merge_file = fopen(all_flags->merge.flag_value, "w");
+    if (all_flags->merge.raised != false) {
+        files.merge_file = fopen(all_flags->merge.value, "w");
         fprintf(files.merge_file, ">Sequences_Merged\n");
         iterate_over_lines(&files, all_flags, &reads, &gc_data, &occurence_count);
         print_output(all_flags, &gc_data, occurence_count, &files, false);
@@ -336,16 +320,15 @@ void parse_fasta(struct All_flags* all_flags) {
         iterate_over_lines(&files, all_flags, &reads, &gc_data, &occurence_count);
         print_output(all_flags, &gc_data, occurence_count, &files, false);
     }
-    if (all_flags->file_out.flag_raised != false) {
+    if (all_flags->file_out.raised != false) {
         fclose(files.out_file);
     }
-    
+
     fclose(files.fasta_file);
 }
 
-
 int main(int argc, char** argv) {
-    struct All_flags all_flags = {0};
+    All_flags all_flags = {0};
 
     int error_flag = 0;
 
